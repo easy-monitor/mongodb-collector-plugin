db7d6f37e4d0957d866349107865ede8e26bd9b9
easyopscyrilchen <cyrilchen@easyops.cn>
2020-10-10 10:59:50 +0800

    fix(script): start exporter under the "script" directory

--- script/deploy/start_script.sh
+++ script/deploy/start_script.sh
@@ -2,7 +2,7 @@
 
 
 PACKAGE_NAME=mongodb-collector-plugin
-PACKAGE_PATH=$(dirname $(dirname "$(cd `dirname $0`; pwd)"))
+PACKAGE_PATH=$(dirname "$(cd `dirname $0`; pwd)")
 LOG_DIRECTORY=$PACKAGE_PATH/log
 LOG_FILE=$LOG_DIRECTORY/$PACKAGE_NAME.log
 
@@ -150,6 +150,6 @@ message="start exporter"
 echo "[INFO] Message: $message"
 echo "$(date "+%Y-%m-%d %H:%M:%S") [INFO] Message: $message" >> $LOG_FILE
 
-cd $PACKAGE_PATH/script
+cd $PACKAGE_PATH
 chmod +x src/mongodb_exporter
 MONGODB_URI=mongodb://$mongodb_user:$mongodb_password@$mongodb_host:$mongodb_port ./src/mongodb_exporter --web.listen-address=$exporter_host:$exporter_port --web.telemetry-path=$exporter_uri 2>&1 | tee -a $LOG_FILE
